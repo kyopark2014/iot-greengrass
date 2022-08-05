a4d0f3aee13314d4aff39f35a80223d4e0a39bdc
ksdyb.park <ksdyb@amazon.com>
2022-08-05 23:33:21 +0900

    update

---
+++ pubsub-iotcore/subsriber/artifacts/com.iotcore.Subscriber/1.0.0/iotcore_subscriber.py
@@ -0,0 +1,54 @@
+import time
+import traceback
+
+import awsiot.greengrasscoreipc
+import awsiot.greengrasscoreipc.client as client
+from awsiot.greengrasscoreipc.model import (
+    SubscribeToTopicRequest,
+    SubscriptionResponseMessage
+)
+
+TIMEOUT = 10
+
+ipc_client = awsiot.greengrasscoreipc.connect()
+                    
+class StreamHandler(client.SubscribeToTopicStreamHandler):
+    def __init__(self):
+        super().__init__()
+
+    def on_stream_event(self, event: SubscriptionResponseMessage) -> None:
+        try:
+            # message_string = str(event.binary_message.message, "utf-8")
+            message_string = event.json_message.message
+            
+            # Handle message.
+            with open('/tmp/Greengrass_IoTCore_Subscriber.log', 'a') as f:
+                print(message_string, file=f)
+
+        except:
+            traceback.print_exc()
+
+    def on_stream_error(self, error: Exception) -> bool:
+        # Handle error.
+        return True  # Return True to close stream, False to keep stream open.
+
+    def on_stream_closed(self) -> None:
+        # Handle close.
+        pass
+
+topic = "core/topic"
+
+request = SubscribeToTopicRequest()
+request.topic = topic
+handler = StreamHandler()
+operation = ipc_client.new_subscribe_to_topic(handler) 
+operation.activate(request)
+future_response = operation.get_response()
+future_response.result(TIMEOUT)
+
+# Keep the main thread alive, or the process will exit.
+while True:
+    time.sleep(10)
+    
+# To stop subscribing, close the operation stream.
+operation.close()
