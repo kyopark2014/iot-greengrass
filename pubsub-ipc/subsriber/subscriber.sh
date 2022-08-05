d22ae2d67bcf0a59b6a33466e7f7b37b5faf6280
ksdyb.park <ksdyb@amazon.com>
2022-08-05 18:31:17 +0900

    update

--- pubsub-ipc/subsriber/subscriber.sh
+++ pubsub-ipc/subsriber/subscriber.sh
@@ -1,4 +1,5 @@
 sudo /greengrass/v2/bin/greengrass-cli deployment create \
   --recipeDir ./recipes \
   --artifactDir ./artifacts \
-  --merge "com.example.Publisher=1.0.0"
+  --merge "com.example.Subscriber=1.0.0"
+  
