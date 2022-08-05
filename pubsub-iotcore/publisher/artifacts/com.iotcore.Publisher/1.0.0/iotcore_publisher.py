8fafefbc66e86882a85565475ef47670f0508962
ksdyb.park <ksdyb@amazon.com>
2022-08-05 22:08:07 +0900

    update

--- pubsub-iotcore/publisher/artifacts/com.iotcore.Publisher/1.0.0/iotcore_publisher.py
+++ pubsub-iotcore/publisher/artifacts/com.iotcore.Publisher/1.0.0/iotcore_publisher.py
@@ -1,5 +1,10 @@
+import time
+import datetime
+import json
+
 import awsiot.greengrasscoreipc
 import awsiot.greengrasscoreipc.client as client
+
 from awsiot.greengrasscoreipc.model import (
     QOS,
     PublishToIoTCoreRequest
@@ -9,15 +14,23 @@ TIMEOUT = 10
 
 ipc_client = awsiot.greengrasscoreipc.connect()
                     
-topic = "my/topic"
-message = "Hello, World"
-qos = QOS.AT_LEAST_ONCE
+topic = "core/topic"
+cnt = 1
 
-request = PublishToIoTCoreRequest()
-request.topic_name = topic
-request.payload = bytes(message, "utf-8")
-request.qos = qos
-operation = ipc_client.new_publish_to_iot_core()
-operation.activate(request)
-future_response = operation.get_response()
-future_response.result(TIMEOUT)
+while True: 
+    message = {"timestamp": str(datetime.datetime.now()),
+               "value": cnt}
+    message_json = json.dumps(message).encode('utf-8')
+    cnt = cnt + 1
+    qos = QOS.AT_LEAST_ONCE
+
+    request = PublishToIoTCoreRequest()
+    request.topic_name = topic
+    request.payload = bytes(message, "utf-8")
+    request.qos = qos
+    operation = ipc_client.new_publish_to_iot_core()
+    operation.activate(request)
+    future_response = operation.get_response()
+    future_response.result(TIMEOUT)
+
+    print(f"publish: {message_json}")
